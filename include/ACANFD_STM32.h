a6f6766ab3a5e3fb5173a78bb24657fb3b1720b8
skse <nazcar101@gmail.com>
2025-06-09 23:11:02 -0400

    platformio update

--- include/ACANFD_STM32.h
+++ include/ACANFD_STM32.h
@@ -6,7 +6,7 @@
 
 #if defined(ARDUINO_NUCLEO_H743ZI2)
   #include "ACANFD_STM32_NUCLEO_H743ZI2-objects.h"
-#elif defined(STM32H750VB_GENERIC)
+#elif defined(GENERICSTM32H750VB)
   #include "ACANFD_STM32_H750VB-objects.h"
 #else
   #error "Unhandled Nucleo Board"
